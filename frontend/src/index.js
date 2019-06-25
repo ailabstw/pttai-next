8c7643925feb0fd58bdfacec299474769cb127cc
poga <poga.bahamut@gmail.com>
2019-06-25 14:19:32 +0800

    cleanup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './build.css'
-import './index.css'
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
