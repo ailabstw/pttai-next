d12f82cc99cdb2a6d1589dc30fb344fa418af697
poga <poga.bahamut@gmail.com>
2019-07-23 10:10:23 +0800

    refactor: ALLOWED_DOMAIN_LIST env var naem

--- backend/auth/google.js
+++ backend/auth/google.js
@@ -1,9 +1,9 @@
 const { OAuth2Client } = require('google-auth-library')
 const GOOGLE_SIGNIN_CLIENT_ID = process.env.GOOGLE_SIGNIN_CLIENT_ID
 
-let DOMAIN_ALLOW_LIST = []
-if (process.env.GOOGLE_SIGNIN_DOMAIN_ALLOW_LIST) {
-  DOMAIN_ALLOW_LIST = process.env.GOOGLE_SIGNIN_DOMAIN_ALLOW_LIST.split(',')
+let ALLOWED_DOMAIN_LIST = []
+if (process.env.GOOGLE_SIGNIN_ALLOWED_DOMAIN_LIST) {
+  ALLOWED_DOMAIN_LIST = process.env.GOOGLE_SIGNIN_ALLOWED_DOMAIN_LIST.split(',')
 }
 
 async function authGoogle (idToken) {
@@ -14,10 +14,10 @@ async function authGoogle (idToken) {
   })
 
   const payload = ticket.getPayload()
-  console.log(DOMAIN_ALLOW_LIST, payload)
+  console.log(ALLOWED_DOMAIN_LIST, payload)
 
-  if (DOMAIN_ALLOW_LIST.length > 0) {
-    if (DOMAIN_ALLOW_LIST.indexOf(payload.hd) === -1) {
+  if (ALLOWED_DOMAIN_LIST.length > 0) {
+    if (ALLOWED_DOMAIN_LIST.indexOf(payload.hd) === -1) {
       throw new Error(`invalid domain: ${payload.hd}`)
     }
   }
