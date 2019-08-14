7c38145d9861ab14fed9d760bc8b513291266a72
poga <poga.bahamut@gmail.com>
2019-08-14 13:09:54 +0800

    extract message component, tweak long message layout

---
+++ frontend/src/Message.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import { MenuProvider } from 'react-contexify'
+import Reactions from './Reactions'
+import Linkify from 'react-linkify'
+import nameColors from './name_colors'
+
+import moment from 'moment'
+
+class Message extends Component {
+  id2color (id) {
+    let n = parseInt(id, 16)
+    return nameColors[n % nameColors.length]
+  }
+
+  render () {
+    let m = this.props.message
+    let shouldRenderDate = true
+    let date = m.date || m.message.date
+
+    if (this.props.lastMessage) {
+      let lastMessage = this.props.lastMessage
+      if (moment(date).date() === moment(lastMessage.date || lastMessage.message.date).date()) {
+        shouldRenderDate = false
+      }
+    }
+
+    let textStyle = ''
+    if (this.props.type === 'action') {
+      textStyle = 'italic text-gray-500'
+    }
+
+    let authorStyle = ''
+    if (this.props.type === 'text') {
+      authorStyle = this.id2color(m.author)
+    }
+
+    return <li
+      className='message flex flex-col hover:bg-gray-100'>
+      {shouldRenderDate
+        ? <div className='text-sm text-center border-b border-gray-300 mb-2'>{moment(date).format('YYYY-MM-DD')}</div>
+        : ''}
+      <div className='flex flex-row justify-between'>
+        <span className='text-gray-600 inline-block mr-4 text-sm w-10'>{moment(date).format('HH:mm')}</span>
+        <span className={textStyle}>
+          <span
+            className={`font-bold cursor-pointer hover:underline ${authorStyle}`}
+            onClick={this.props.onNewFriend(m.author)}>
+            {this.props.author}
+          </span>
+          <Linkify properties={{ target: '_blank', className: 'text-blue-400 underline' }}>{` ${m.message.value}`}</Linkify>
+        </span>
+        {this.props.allowReact ? <MenuProvider id='menu_id' event='onClick' data={m}>
+          <span className='text-gray-500 hover:text-black cursor-pointer'>...</span>
+        </MenuProvider> : ''}
+      </div>
+      { m.reactions && m.reactions.length > 0
+        ? <div className='my-1 mb-3 ml-16'>
+          <Reactions myKey={this.props.myKey} reactions={m.reactions} onAddReaction={this.props.onAddReaction} message={m} />
+        </div>
+        : <div className='my-1' />}
+    </li>
+  }
+}
+
+export default Message
--- frontend/src/Messages.js
+++ frontend/src/Messages.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react'
-import { MenuProvider } from 'react-contexify'
-import Reactions from './Reactions'
-import Linkify from 'react-linkify'
-import nameColors from './name_colors'
 
-import moment from 'moment'
+import Message from './Message'
 
 class Messages extends Component {
   onNewFriend (id) {
     return () => {
       this.props.onNewFriend(id)
     }
-  }
-
-  id2color (id) {
-    let n = parseInt(id, 16)
-    return nameColors[n % nameColors.length]
   }
 
   render () {
@@ -32,63 +23,15 @@ class Messages extends Component {
         : ''}
       {this.props.messages.length > 0 ? <div className='h-48' /> : ''}
       {this.props.messages.map((m, i) => {
-        let shouldRenderDate = true
-        let date = m.date || m.message.date
-
-        if (i > 0) {
-          let lastMessage = this.props.messages[i - 1]
-          if (moment(date).date() === moment(lastMessage.date || lastMessage.message.date).date()) {
-            shouldRenderDate = false
-          }
-        }
-
-        if (m.message.type === 'text') {
-          return <li
-            key={m.id}
-            className='flex flex-col hover:bg-gray-100'>
-            {shouldRenderDate ? <div className='text-sm text-center border-b border-gray-300 mb-2'>{moment(date).format('YYYY-MM-DD')}</div> : ''}
-            <div className='flex flex-row justify-between'>
-              <span className='break-all'>
-                <span className='text-gray-600 inline-block mr-4 text-sm w-10'>{moment(date).format('HH:mm')}</span>
-                <span className={`font-bold cursor-pointer hover:underline ${this.id2color(m.author)}`} onClick={this.onNewFriend(m.author)}>
-                  {m.author ? (this.props.profiles[m.author] ? this.props.profiles[m.author].name.substring(0, 16) : m.author.substring(0, 16) + '...') : ''}
-                </span>
-                : <Linkify properties={{ target: '_blank', className: 'text-blue-600 underline' }}>{m.message.value}</Linkify>
-              </span>
-              {this.props.allowReact ? <MenuProvider id='menu_id' event='onClick' data={m}>
-                <span className='text-gray-500 hover:text-black cursor-pointer'>...</span>
-              </MenuProvider> : ''}
-            </div>
-            { m.reactions && m.reactions.length > 0
-              ? <div className='my-1 mb-3 ml-16'>
-                <Reactions myKey={this.props.myKey} reactions={m.reactions} onAddReaction={this.props.onAddReaction} message={m} />
-              </div>
-              : <div className='my-1' />}
-          </li>
-        } else if (m.message.type === 'action') {
-          return <li
-            key={m.id}
-            className='message flex flex-col hover:bg-gray-100'>
-            <div className='flex flex-row justify-between'>
-              <span className='text-gray-500'>
-                <span className='text-gray-600 inline-block mr-4 text-sm w-10'>{moment(date).format('HH:mm')}</span>
-                <span className='font-bold cursor-pointer hover:underline italic' onClick={this.onNewFriend(m.author)}>
-                  {m.author ? (this.props.profiles[m.author] ? this.props.profiles[m.author].name.substring(0, 16) : m.author.substring(0, 16) + '...') : ''}
-                </span>
-                <span className='italic'><Linkify properties={{ target: '_blank', className: 'text-blue-400 underline' }}>{` ${m.message.value}`}</Linkify></span>
-              </span>
-              {this.props.allowReact ? <MenuProvider id='menu_id' event='onClick' data={m}>
-                <span className='text-gray-500 hover:text-black cursor-pointer'>...</span>
-              </MenuProvider> : ''}
-            </div>
-            { m.reactions && m.reactions.length > 0
-              ? <div className='my-1 mb-3 ml-16'>
-                <Reactions myKey={this.props.myKey} reactions={m.reactions} onAddReaction={this.props.onAddReaction} message={m} />
-              </div>
-              : <div className='my-1' />}
-          </li>
-        }
-        return ''
+        let author = m.author ? (this.props.profiles[m.author] ? this.props.profiles[m.author].name.substring(0, 16) : m.author.substring(0, 16) + '...') : ''
+        return <Message
+          key={m.id}
+          message={m}
+          author={author}
+          lastMessage={i > 0 ? this.props.messages[i - 1] : null}
+          type={m.message.type}
+          allowReact={this.props.allowReact}
+          onNewFriend={this.onNewFriend.bind(this)} />
       })}
     </ul>
   }
